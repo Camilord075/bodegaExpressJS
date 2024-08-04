089d8724889df9c942f3f5d2e1f56956e0849647
Camilo Arteta <99410397+Camilord075@users.noreply.github.com>
2024-08-03 21:07:20 -0500

    Quitando manualmente

--- config.js
+++
@@ -1,8 +0,0 @@
-export const {
-    DB_HOST = 'localhost',
-    DB_USERNAME = 'root',
-    DB_PORT = 3306,
-    DB_PASS = 'Cami0707',
-    DB_NAME = 'appbodtest1',
-    APP_PORT = 3000
-} = process.env
